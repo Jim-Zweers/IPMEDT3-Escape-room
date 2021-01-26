96e805f5b1898f550fbefab8661bbdc9a10f95ef
Jim-Zweers <60381427+Jim-Zweers@users.noreply.github.com>
2021-01-26 15:23:28 +0100

    Plaatsen mogelijk
    Het plaatsen van de lettergrepen is nu mogelijk. Er moet nog een uitleg worden toegevoegd en een pop up

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,11 @@ window.onload = () =>{
     const places = document.getElementsByClassName('js--place');
     const camera = document.getElementById('js--camera');
     const cursor = document.getElementById('js--cursor');
+    const pickups = document.getElementsByClassName('js--pickup');
+    let hold = null;
+    const placeholders = document.getElementsByClassName('js--placeholder');
+    let scene = document.getElementById('js--scene');
+
 
     for (let i = 0; i < places.length; i++) {
         places[i].addEventListener('click', function(evt){
@@ -11,6 +16,40 @@ window.onload = () =>{
           att.value = "property: position; easing: linear; dur: " + tijd + "; to: " + this.getAttribute('position').x + " 6 " + this.getAttribute('position').z;
           camera.setAttribute('animation', att.value);
           sound.components.sound.playSound();
+        });
+      }
+
+      function addListeners(){
+        for (let i = 0; i < pickups.length; i++){
+          pickups[i].addEventListener('click', function(evt){
+            if (hold == null) {
+            camera.innerHTML += '<a-box id="js--hold" class="js--pickup js--interact" color="'+ this.getAttribute("color") + '" height='+ this.getAttribute("height") +' rotation="0 0 -90" position="2 -2 -4" depth="0.1">'+ this.innerHTML + '</a-box>'
+            hold = "box";
+            this.remove();
+            }
+          });
+        }
+      }
+    
+      addListeners()
+    
+      for (let i = 0; i < placeholders.length; i++){
+        placeholders[i].addEventListener('click', function(evt){
+          if (hold == "box"){
+            let heldbox = document.getElementById('js--hold')
+            let box = document.createElement('a-box');
+            box.setAttribute("class", "js--pickup js--interact");
+            box.setAttribute("color", heldbox.getAttribute("color"));
+            box.setAttribute("height", heldbox.getAttribute("height"));
+            box.setAttribute("rotation", "0 90 -90");
+            box.setAttribute("depth", "0.1")
+            box.innerHTML += heldbox.innerHTML;
+            box.setAttribute("position", {x: "-6", y:"4.5", z: this.getAttribute('position').z});
+            scene.appendChild(box);
+            document.getElementById('js--hold').remove();
+            addListeners();
+            hold = null;
+          }
         });
       }
 
